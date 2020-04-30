746873713950b880e7b21c3efa0cc9d4ec19184f
mamounchaoui <rcmamoun@hotmail.com>
2020-05-01 00:00:03 +0200

    fichier pour tester les fonctions

---
+++ test/main.c
@@ -0,0 +1,21 @@
+
+#include "chercheurdemploi.h"
+#include "employe.h"
+#include "entreprise.h"
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+     // tests pour 'entreprise'
+
+     // tests pour 'chercheurdemploi'
+
+     // tests pour 'employe'
+     return 0;
+  
+}
