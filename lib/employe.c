fe0ec168a4eabf308e9ee2cbcc667e6dde03db85
mamounchaoui <rcmamoun@hotmail.com>
2020-04-30 23:56:01 +0200

    fonctions de départ pour employe

---
+++ lib/employe.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "employe.h"
+
+
+int nbrligne2(FILE* dbb)
+{
+    return 0;
+}
+void Creer_profil2()
+{
+    return;
+}
+void Modifier_profil2()
+{
+    return;
+}
+void Transitionner_profil2()
+{
+    return;
+}
+void Supprimer_profil2()
+{
+    return;
+}
+void Recherche_par_poste2()
+{
+    return;
+}
+void Rechercher_par_anciencollegue2()
+{
+    return;
+}
