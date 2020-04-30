8a3029d2cfe5fdda511b3783f6cf433ded723590
root <root@DESKTOP-5F7QMNL.localdomain>
2020-04-30 23:41:41 +0200

    Ajout de entreprise.h et entreprise.c pour la gestion des entreprises

---
+++ lib/entreprise.c
@@ -0,0 +1,37 @@
+#include "entreprise.h"
+#include <stdlib.h>
+#include "liste.h"
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+void creer_profil_entreprise(FILE* fic, entreprise* ent)
+{
+    return ;
+}
+
+
+void supprimer_entreprise(FILE* fic, entreprise* ent)
+{
+    return ;
+}
+
+void creer_poste_a_pourvoir(FILE* fic, poste* unposte){
+    return ;
+}
+
+void supprimer_poste(FILE* fic, char* titre)
+{
+    return ;
+}
+
+int trouver_nom_ent(FILE* fic)
+{
+    return 0;
+}
+
+int trouver_poste_par_competence(FILE* fic, char* competence)
+{
+    return 0;
+}
---
+++ lib/entreprise.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "liste.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+
+typedef struct entreprise
+{
+    int index;
+    char nom[128];
+    char codepost[128];
+    char courriel[128];
+} entreprise;
+
+#define MAX_ENT 5
+
+typedef struct poste
+{
+    int index;
+    char titre[128];
+    char competences[128];
+    struct entreprise *ent[MAX_ENT];
+} poste;
+
+
+void creer_profil_entreprise(FILE* fic, entreprise* ent);
+void creer_poste_a_pourvoir(FILE* fic, poste* unposte);
+void supprimer_entreprise(FILE* fic, char* nom_ent);
+void supprimer_poste(FILE* fic, char* titre);
+int trouver_nom_ent(FILE* fic);
+int trouver_poste_par_competence(FILE* fic, char* competence);
+
