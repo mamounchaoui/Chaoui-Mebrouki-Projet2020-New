3f24d50293f9473369545220422e8691ce0a551d
mamounchaoui <rcmamoun@hotmail.com>
2020-04-30 23:56:42 +0200

    prototypes de employe.c

---
+++ lib/employe.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+int nbrligne2(FILE* dbb);
+void Creer_profil2();
+void Modifier_profil2();
+void Transitionner_profil2();
+void Supprimer_profil2();
+void Recherche_par_poste2();
+void Rechercher_par_anciencollegue2();
