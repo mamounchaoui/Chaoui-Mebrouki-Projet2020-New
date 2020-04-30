19ada4a15676f7593a26fa3a9391072fd8311390
mamounchaoui <rcmamoun@hotmail.com>
2020-04-30 23:55:02 +0200

    prototypes de chercheurdemploi.c

---
+++ lib/chercheurdemploi.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+int nbrligne(FILE* dbb);
+void Creer_profil();
+void Modifier_profil();
+void Transitionner_profil();
+void Supprimer_profil();
+void Recherche_par_poste();
+void Rechercher_par_anciencollegue();
+
