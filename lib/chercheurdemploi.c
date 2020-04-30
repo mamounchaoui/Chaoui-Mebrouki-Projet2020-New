a18dc4eb00470546b8dfd122fdda6a6024e7bfe0
mamounchaoui <rcmamoun@hotmail.com>
2020-04-30 23:53:33 +0200

    fonctions de départ pour chercheurdemploi

---
+++ lib/chercheurdemploi.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "chercheurdemploi.h"
+
+
+int nbrligne(FILE* dbb)
+{
+    return 0;
+}
+
+void Creer_profil()
+{
+    return;
+}
+
+void Modifier_profil()
+{
+    return;
+}
+
+void Transitionner_profil()
+{
+    return;
+}
+
+void Supprimer_profil()
+{
+    return;
+}
+
+void Recherche_par_poste()
+{
+    return
+}
+
+void Rechercher_par_anciencollegue()
+{
+
+}
