079e0e340898f739a887ac159905082168043fc5
mamounchaoui <rcmamoun@hotmail.com>
2020-05-02 00:59:02 +0200

    modification du protoype de Creer_profil

--- lib/chercheurdemploi.c
+++ lib/chercheurdemploi.c
@@ -9,7 +9,7 @@ int nbrligne(FILE* dbb)
     return 0;
 }
 
-void Creer_profil()
+void Creer_profil(char nom[128], char prenom[128],char mail[128], char code_postal[128], char competence[128], char collegue[128])
 {
     return;
 }
--- lib/chercheurdemploi.h
+++ lib/chercheurdemploi.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int nbrligne(FILE* dbb);
-void Creer_profil();
+void Creer_profil(char nom[128], char prenom[128],char mail[128], char code_postal[128], char competence[128], char collegue[128]);
 void Modifier_profil();
 void Transitionner_profil();
 void Supprimer_profil();
