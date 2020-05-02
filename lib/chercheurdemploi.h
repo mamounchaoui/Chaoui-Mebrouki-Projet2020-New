537c9374c8dd572830e81f53d5a33475af0ac42c
mamounchaoui <rcmamoun@hotmail.com>
2020-05-02 18:48:59 +0200

    modification des prototypes de Transitionner_profil et Supprimer_profil

--- lib/chercheurdemploi.h
+++ lib/chercheurdemploi.h
@@ -5,8 +5,8 @@
 int nbrligne(FILE* dbb);
 void Creer_profil(char nom[128], char prenom[128],char mail[128], char code_postal[128], char competence[128], char collegue[128]);
 void Modifier_profil();
-void Transitionner_profil();
-void Supprimer_profil();
+void Transitionner_profil(char nom[128], char prenom[128], char entrep[128]);
+void Supprimer_profil(char nom[128], char prenom[128]);
 void Recherche_par_poste();
 void Rechercher_par_anciencollegue();
 
