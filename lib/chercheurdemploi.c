b7e102a13a95037da190effce73be1f58c22fd6c
mamounchaoui <rcmamoun@hotmail.com>
2020-05-02 18:51:32 +0200

    implementation de Transitionner_profil et Supprimer_profil

--- lib/chercheurdemploi.c
+++ lib/chercheurdemploi.c
@@ -99,13 +99,253 @@ void Modifier_profil()
     return;
 }
 
-void Transitionner_profil()
+void Transitionner_profil(char nom[128], char prenom[128], char entrep[128])
 {
+    int i,t,y,j,z,k,x,a;
+    char str[128];
+    char str1[128];
+    char ajout[128];
+    x=0;
+    FILE* db=fopen("test/chercheurdemploi.csv", "r+");
+    int w=nbrligne(db);
+    FILE* fp;
+    fp=fopen("test/tmp.csv", "w");
+    fseek(db,0,SEEK_SET);
+    
+    FILE* db2=fopen("test/entreprise.csv", "r+");
+    y=nbrligne(db2);
+    fseek(db2,0,SEEK_SET);
+    fscanf(db2,"%*s");
+    char* temp1,*temp2;
+    a=0;
+    int o,d,e;
+    while (a<y)
+    {
+        fscanf(db2,"%s",str1);
+        o=0;
+        d=0;
+        e=0;
+        temp1=strtok(str1,",");
+        temp2=strtok(NULL,",");
+        while (temp2[o]==entrep[o] && temp2[o]!='\0')
+        {
+            o++;
+            d++;
+        }
+
+        if (d==strlen(entrep))
+        {
+            e=1;
+            a=y;
+        }
+        a++;
+        if (a==y && e==0)
+        {
+            temp1="";
+        }
+            
+    }
+    
+
+    char* t2,*t3,*t4,*t5,*t6,*t7;
+    fscanf(db,"%s",str);
+    printf("%s\n",str);
+    char* v;
+    int ok;
+    v=strtok(str,",");
+    while (v!=NULL && x<w )
+    {
+        ok=0;
+        v=strtok(NULL,",");
+        i=0;
+        y=0;
+        while (v[i]==nom[i] && v[i]!='\0')
+        {
+            y++;
+            i++;
+
+        }
+        j=0;
+        z=0;
+        v=strtok(NULL,",");
+        while (v[j]==prenom[j] && v[j]!='\0')
+        {
+            z++;
+            j++;
+
+        }
+        t=strlen(nom);
+        k=strlen(prenom);
+        if (y==t && z==k)
+        {
+            ok=1;
+            fseek(db,0,SEEK_SET);
+            fscanf(db,"%s",str);
+            fprintf(fp,"%s",str);
+            int m=1;
+            while (m<w-1)
+            {   
+                if (m==x)
+                {
+                    fscanf(db,"%s",str);
+                }
+                             
+                fscanf(db,"%s",str);
+                // printf("%s\n",str);
+                t2=strtok(str,",");
+                t2=strtok(NULL,",");
+                t3=strtok(NULL,",");
+                t4=strtok(NULL,",");
+                t5=strtok(NULL,",");
+                t6=strtok(NULL,",");
+                t7=strtok(NULL,",");
+                
+                fprintf(fp,"\n%d,%s,%s,%s,%s,%s,%s", m, t2, t3, t4, t5, t6, t7);
+                m++;
+                
+                
+            }
+            
+            goto fin;
+        }
+        else
+        {
+            goto next;
+        }
+        next:
+        fscanf(db,"%s",str);
+        printf("%s\n",str);
+        strcpy(ajout,str);
+        printf("%s\n",ajout);
+        
+        x++;
+        v=strtok(str,",");
+    
+        if (x==w && ok==0)
+        {
+            printf("on arrive pas à vous identifier\n");
+            goto fin2;
+        }
+        
+    }
+    fin:
+    fclose(db);
+    fclose(fp);
+    FILE* db1=fopen("test/employe.csv", "r+");
+    
+    printf("%s\n",ajout);
+    t2=strtok(ajout,",");
+    t2=strtok(NULL,",");
+    t3=strtok(NULL,",");
+    t4=strtok(NULL,",");
+    t5=strtok(NULL,",");
+    t6=strtok(NULL,",");
+    t7=strtok(NULL,",");
+    int f;
+    f=nbrligne(db1);
+    printf("%d\n",f);
+    fseek(db1,0,SEEK_CUR);
+    // id,nom,prenom,mail,code_postal,competences,collegues,entreprise
+    fprintf(db1,"\n%d,%s,%s,%s,%s,%s,%s,%s",f,t2,t3,t4,t5,t6,t7,temp1);
+    fclose(db1);
+    remove("test/chercheurdemploi.csv");
+    rename("test/tmp.csv","test/chercheurdemploi.csv");
+    fin2:
     return;
 }
 
-void Supprimer_profil()
+void Supprimer_profil(char nom[128], char prenom[128])
 {
+    int i,t,y,j,z,k,x;
+    char str[128];
+    x=0;
+    FILE* db=fopen("test/chercheurdemploi.csv", "r+");
+    int w=nbrligne(db);
+    FILE* fp;
+    fp=fopen("test/tmp.csv", "w");
+    fseek(db,0,SEEK_SET);
+    char* t2,*t3,*t4,*t5,*t6,*t7;
+    fscanf(db,"%s",str);
+
+    char* v;
+    int ok;
+    v=strtok(str,",");
+    while (v!=NULL && x<w )
+    {
+        ok=0;
+        v=strtok(NULL,",");
+        i=0;
+        y=0;
+        while (v[i]==nom[i] && v[i]!='\0')
+        {
+            y++;
+            i++;
+
+        }
+        j=0;
+        z=0;
+        v=strtok(NULL,",");
+        while (v[j]==prenom[j] && v[j]!='\0')
+        {
+            z++;
+            j++;
+
+        }
+        t=strlen(nom);
+        k=strlen(prenom);
+        if (y==t && z==k)
+        {
+            ok=1;
+            fseek(db,0,SEEK_SET);
+            fscanf(db,"%s",str);
+            fprintf(fp,"%s",str);
+            int m=1;
+            while (m<w-1)
+            {   
+                if (m==x)
+                {
+                    fscanf(db,"%s",str);
+                }
+                             
+                fscanf(db,"%s",str);
+                t2=strtok(str,",");
+                t2=strtok(NULL,",");
+                t3=strtok(NULL,",");
+                t4=strtok(NULL,",");
+                t5=strtok(NULL,",");
+                t6=strtok(NULL,",");
+                t7=strtok(NULL,",");
+                
+                fprintf(fp,"\n%d,%s,%s,%s,%s,%s,%s", m, t2, t3, t4, t5, t6, t7);
+                m++;
+                
+                
+            }
+            
+            goto fin;
+        }
+        else
+        {
+            goto next;
+        }
+        next:
+        // fscanf(db,"%*s");
+        fscanf(db,"%s",str);
+        x++;
+        v=strtok(str,",");
+        if (x==w && ok==0)
+        {
+            printf("on arrive pas à vous identifier\n");
+            goto fin2;
+        }
+        
+    }
+    fin:
+    fclose(db);
+    fclose(fp);
+    remove("test/chercheurdemploi.csv");
+    rename("test/tmp.csv","test/chercheurdemploi.csv");
+    fin2:
     return;
 }
 
