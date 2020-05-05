e53e8c45842c4684f831703357b44283a4845ad9
mamounchaoui <rcmamoun@hotmail.com>
2020-05-05 04:44:49 +0200

    modification des prototypes de Modifier_profil et Recherche_par_poste

--- lib/chercheurdemploi.h
+++ lib/chercheurdemploi.h
@@ -4,9 +4,9 @@
 
 int nbrligne(FILE* dbb);
 void Creer_profil(char nom[128], char prenom[128],char mail[128], char code_postal[128], char competence[128], char collegue[128]);
-void Modifier_profil();
+void Modifier_profil(char nom[128], char prenom[128], char compe[128], char col[128], char code[128], int choix);
 void Transitionner_profil(char nom[128], char prenom[128], char entrep[128]);
 void Supprimer_profil(char nom[128], char prenom[128]);
-void Recherche_par_poste();
+void Recherche_par_poste(char nom[128], char prenom[128], int choix);
 void Rechercher_par_anciencollegue();
 
