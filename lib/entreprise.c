b0a93949ad33e4844be76e9efee9c88b85e608e1
root <root@DESKTOP-5F7QMNL.localdomain>
2020-05-16 21:54:22 +0200

    Implémentation de créer profil entreprise

--- lib/entreprise.c
+++ lib/entreprise.c
@@ -7,7 +7,37 @@
 
 void creer_profil_entreprise(FILE* fic, entreprise* ent)
 {
-    return ;
+    int l=0,compt=1;
+    //char nom[128],codep[128],cour[128];
+    char debut[50] = "id,nom,code postal,mail\n";
+    char chunk[128];
+
+    fputs(debut,fic);
+    fseek(fic,0,SEEK_SET);
+
+    while(fgets(chunk, sizeof(chunk), fic) != NULL) {
+         if(chunk[0] == '\n'){
+            fseek(fic,-1,SEEK_CUR);
+            goto here;
+        }
+        //fputs(chunk, stdout);
+        l++;
+     }
+here:
+    while(compt != 5){
+        switch(compt){
+            case 1: fprintf(fic,"%d",l); break; 
+            case 2: fputs(ent->nom,fic); break; 
+            case 3: fputs(ent->codepost,fic); break;
+            case 4: fputs(ent->courriel,fic);break;
+        }
+        if(compt < 4){
+            fputs(",",fic);
+        }
+        compt++;
+    }
+    fputs("\n",fic);
+    
 }
 
 
